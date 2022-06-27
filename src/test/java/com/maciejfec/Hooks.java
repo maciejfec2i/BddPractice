aa40f396b3d8ff3a6a6ba1cff28d1670e792e84c
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-27 15:51:15 +0100

    added a setUp and tearDown methods. Added a getter for the driver

--- src/test/java/com/maciejfec/Hooks.java
+++ src/test/java/com/maciejfec/Hooks.java
@@ -1,5 +1,55 @@
 package com.maciejfec;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Hooks {
-    
+
+    private WebDriver driver;
+    @Before
+    public void setUp() {
+
+        String browser = System.getProperty("browser");
+
+        if(browser == null) {
+            browser = "";
+        }
+
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    public WebDriver getDriver() {
+
+        return driver;
+    }
 }
