41376ba94c59ffff6b4cc6c2566a4ade2feaf5d4
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-27 16:43:23 +0100

    added setUp and tearDown methods

--- src/test/java/com/maciejfec/BaseClass.java
+++ src/test/java/com/maciejfec/BaseClass.java
@@ -1,2 +1,59 @@
-package com.maciejfec;public class BaseClass {
+package com.maciejfec;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseClass {
+
+    private WebDriver driver;
+    private String baseUrl = "https://www.saucedemo.com/";
+
+    public void setUp() {
+
+        String browser = System.getProperty("browser");
+
+        if(browser == null) {
+            browser = "";
+        }
+
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
+
+    public void tearDown() {
+
+        driver.quit();
+    }
+
+    public WebDriver getDriver() {
+
+        return driver;
+    }
+
+    public String getBaseUrl() {
+
+        return baseUrl;
+    }
 }
