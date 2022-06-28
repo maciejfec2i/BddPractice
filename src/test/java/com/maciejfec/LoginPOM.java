8fdea24921b0820624c11d57536abe5046404c57
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-28 09:08:41 +0100

    created page object model

--- src/test/java/com/maciejfec/LoginPOM.java
+++ src/test/java/com/maciejfec/LoginPOM.java
@@ -1,4 +1,48 @@
 package com.maciejfec;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPOM {
+
+    private WebDriver driver;
+
+    public LoginPOM(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = "input[data-test='username']")
+    private WebElement usernameField;
+
+    @FindBy(css = "input[data-test='password']")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginBtn;
+
+    public void inputUsername(String username) {
+
+        usernameField.sendKeys(username);
+    }
+
+    public void inputPassword(String password) {
+
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginBtn() {
+
+        loginBtn.click();
+    }
+
+    public void login(String username, String password) {
+
+        inputUsername(username);
+        inputPassword(password);
+        clickLoginBtn();
+    }
 }
